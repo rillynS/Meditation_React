b0821e88f2f26604a0e6893ca4f4de99d4ab134e
rilleynS <POM@m.sv-plus.ru>
2021-08-12 23:23:29 +0200

    finished Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Button from '../../hoc/Button/Button';
 import Wrapper from "../../hoc/Wrapper/Wrapper";
 import classes from './Main.module.css';
-
+import GirlImg from '../../img/girl_main.png';
 export default function Main (props) {
     return(
         <div className={classes.Main}>
             <Wrapper>
                 <h1>Meditations Helps You Detoxify Your Body</h1>
                 <p>“It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout point of using Lorem Ipsum is that it has a more-or-less.”</p>
-                <button>Get Start </button>
+                <Button title='Get started' style={{padding:'19px 89px'}}/>
+                <img src={GirlImg}alt='girl_main_img'/>
             </Wrapper>
         </div>
     )
